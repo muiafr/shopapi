d32ab892491e1530dfe41a5464ce4c2488aa7db5
lmkptr <dinqfh@gmail.com>
2026-09-22 18:06:20 +0200

    added new commands

--- src/routes/users/users.py
+++ src/routes/users/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -9,10 +9,42 @@ from src.schemas.users.user import UserCreate, UserUpdate, UserRead
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/", response_model=list[UserRead])
+@router.get("/", response_model=list[UserRead], status_code=200)
 async def get_users(db: Session = Depends(get_db) ):
     users = db.scalars(select(Users)).all()
     return users
 
 
 
+@router.get('/{id}', response_model=UserRead, status_code=200)
+async def get_user(id: int, db: Session = Depends(get_db)):
+        user = db.query(Users).filter(Users.id == id).first()
+        return user
+
+
+@router.post("/create_user", response_model=UserRead, status_code=200)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = Users(
+        nickname = user.nickname,
+        firstname = user.firstname,
+        lastname = user.lastname,
+        phone = user.phone,
+        email = user.email,
+        password = user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.delete("/{id}", response_model=UserRead, status_code=200)
+async def delete_user(id: int, db: Session = Depends(get_db)):
+    user=db.get(Users,id)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db.delete(user)
+    db.commit()
+
+    return Response(status_code=204)
